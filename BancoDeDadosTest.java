96a766244205851438c2357da77f817d117ecfff
janpessoa <janaylsonpessoa@gmail.com>
2022-05-31 17:14:19 -0300

    atualização do teste junit

---
+++ BancoDeDadosTest.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class BancoDeDadosTest {
+
+    private BancoDeDados db;
+
+    @Before
+    public void init() {
+        db = new BancoDeDados();
+    }
+ 
+    @Test
+    public void verificaTamanhoDaListaAoAdicionarAluno() {
+        // Cenário
+
+        Aluno aluno = new Aluno(4, "Neville Longbottom", "Sistemas da Informação");
+
+        // Ação
+
+        db.adicionarAluno(aluno);
+
+        // Validação
+
+        Assert.assertEquals("4", String.valueOf(db.getAlunos().size()));
+    }
+
+    @Test
+    public void verificaTamanhoDaListaAoRemoverAluno() {
+        // Cenário
+
+        int alunoId = 2;
+
+        // Ação
+
+        db.deletarAluno(alunoId);
+
+        // Validação
+
+        Assert.assertEquals("2", String.valueOf(db.getAlunos().size()));
+    }
+
+
+}
